eadabda75d9b896aac99bc70edf5ea066ed5aa68
helouazizi <ouazizi2code@gmail.com>
2025-04-13 00:56:57 +0100

    add store and kload data

--- internal/repository/store.go
+++ internal/repository/store.go
@@ -4,6 +4,7 @@ import (
 	"sync"
 
 	"go-rest-api/internal/models"
+	"go-rest-api/internal/utils"
 )
 
 type Store struct {
@@ -17,4 +18,24 @@ type Store struct {
 
 func New_Store() *Store {
 	return &Store{}
+}
+
+// loadd data
+func (s *Store) LoadData() {
+	apiUrls := []string{
+		"https://groupietrackers.herokuapp.com/api/artists",
+		"https://groupietrackers.herokuapp.com/api/locations",
+		"https://groupietrackers.herokuapp.com/api/dates",
+		"https://groupietrackers.herokuapp.com/api/relation",
+	}
+	s.Wg.Add(len(apiUrls))
+	go utils.Fetch(apiUrls[0], &s.Artists, &s.Wg)
+	go utils.Fetch(apiUrls[1], &s.Locations, &s.Wg)
+	go utils.Fetch(apiUrls[2], &s.Dates, &s.Wg)
+	go utils.Fetch(apiUrls[3], &s.Realtions, &s.Wg)
+	s.Wg.Wait()
+	// fmt.Println("Fetched Artists Data:", s.Artists[0])
+	// fmt.Println("Fetched Locations Data:", s.Locations.Index[0])
+	// fmt.Println("Fetched Dates Data:", s.Dates.Index)
+	// fmt.Println("Fetched Relation Data:", s.Realtions.Index)
 }
--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -2,8 +2,40 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+	"sync"
+
+	"go-rest-api/pkg/logger"
 )
+
+// FetchJSON fetches JSON from a given URL
+func Fetch(url string, target any, wg *sync.WaitGroup) {
+	defer wg.Done()
+	resp, err := http.Get(url)
+	if err != nil {
+		logger.LogWithDetails(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		logger.LogWithDetails(fmt.Errorf("failed to fetch data from %s: StatusCode %d", url, resp.StatusCode))
+		return
+	}
+	// Read the response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		logger.LogWithDetails(err)
+		return
+	}
+	// Parse the JSON response into the appropriate struct
+	if err := json.Unmarshal(body, &target); err != nil {
+		logger.LogWithDetails(err)
+		return
+	}
+}
 
 // Helper function to standardize error responses
 func RespondWithError(w http.ResponseWriter, statusCode int, message, details string) {
