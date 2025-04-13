e2a397ee5201b1981a8351153519e4e3f4f19b0a
helouazizi <ouazizi2code@gmail.com>
2025-04-13 09:34:30 +0100

    add cors zidleware

---
+++ internal/middlewares/cors_middleware.go
@@ -0,0 +1,19 @@
+package middlewares
+
+import "net/http"
+
+func CORSMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*") // Allow all origins — for dev
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		// Handle preflight request
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
